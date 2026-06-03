51cb13ddf6e7821630f1a98a5c50a46ac66626c3
shiv578 <swarajrkfl22@gmail.com>
2026-06-03 21:34:31 +0530

    added

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -62,7 +62,7 @@
                             @csrf
     
                                <x-dropdown-link :href="route('logout')"
-                                      onclick="event.preventDefault();
+                                         onclick="event.preventDefault();
    
                                                     this.closest('form').submit();">
                                 {{ __('Log Out') }}
